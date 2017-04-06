13d0d6f1081b6ef0d593fc183c9ec27dc632081d
bbarres <benoit.barres@gmail.com>
2017-04-06 13:56:41 +0200

    some improvement made to the distribution in nat pop figure

--- distri_resist.R
+++ distri_resist.R
@@ -14,12 +14,13 @@ plot(dnorm(seq(-3,3,length=100)))
 
 #the distribution of the quantitative resistance in a natural population
 curve(dnorm(x, mean=20, sd=6),from=0,to=40,lwd=5,
-      xlab="concentration de pesticide supportée",
+      xlab="Niveau de résistance",
       ylab="Fréquence dans la population",n=10000,
-      cex.lab=2,bty="l",main="POPULATION NATUREL",cex.main=3, 
-      cex.axis=4)
+      cex.lab=1,bty="l",main="DISTRIBUTION DES INDIVIDUS DANS UNE 
+      POPULATION NATUREL EN FONCTION 
+      DE LEUR NIVEAU DE RESISTANCE A UN PESTICIDE",cex.main=1,cex.axis=1)
 #adding the mean on the plot
-segments(20,0,20,dnorm(0,sd=6), col='blue', lwd=4)
+segments(20,0,20,dnorm(0,sd=6),col='blue',lwd=4)
 #adding the 
 M<-20
 EcarT<-6
@@ -29,13 +30,18 @@ X<-seq(xmin, xmax, length=1000)
 Y<-dnorm(X, mean=M, sd=EcarT)
 XX<-c(xmin,X,xmax,xmax,xmin)
 YY<-c(0,Y,dnorm(xmax, mean=M,sd=EcarT),0,0)
-polygon(XX,YY, density=20, border = 'black')
+polygon(XX,YY, border = 0,col="red")
+
+xmin<-0
+xmax<-8
+X<-seq(xmin, xmax, length=1000)
+Y<-dnorm(X, mean=M, sd=EcarT)
+XX<-c(xmin,X,xmax,xmax,xmin)
+YY<-c(0,Y,dnorm(xmax, mean=M,sd=EcarT),0,0)
+polygon(XX,YY, border = 0,col="green4")
 
 
-
-
-
-
+curve(dnorm(x, mean=20, sd=6),from=0,to=40,lwd=5,add=TRUE)
 
 
 
